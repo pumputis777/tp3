603bd230eada2f46efe9a3fef6f1e1bcf15f322c
pumputis777 <facundopumputis@gmail.com>
2025-05-29 08:59:25 -0300

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int n;
+    int impar = 0;
+    int par = 0;
+    int i = 0;
+    int duda;
+    int num1;
+ cout << "cuantos valores quiere ingresar: ";
+ cin >> n;
+ 
+ while (i < n)
+ {
+    cout << "ingrese el numero: " <<endl;  
+    cin >> num1;
+    
+ duda = num1 % 2;
+ 
+ if (duda == 0)
+ {
+    par = par + 1;
+ }
+ else if (duda == 1)
+ {
+    impar = impar + 1;  
+ }
+    i = i +1; 
+ }
+ cout << "la cantidad de impares es de: " << impar << endl;
+ cout << "la cantidad de pares es de: " << par << endl;
+ return 0;
+}
