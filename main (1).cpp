4182fa4b3b05ef91362428d513f0b3a66711f907
pumputis777 <facundopumputis@gmail.com>
2025-05-29 08:40:16 -0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int i = 0;    
+    int num1 = 11;
+    int num2 = 22;
+    int num3 = 33;
+    int num4 = 44;
+ 
+ while (i < 25)
+ {
+    i = i + 1;
+    cout << num1 <<endl;
+    cout << num2 <<endl;
+    cout << num3 <<endl;
+    cout << num4 <<endl;
+    cout << i <<endl;
+ }
+    return 0;
+}
